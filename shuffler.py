a2c559f840fa5487037cb9b5abebac1339016761
Douglas Bagnall <douglas@halo.gen.nz>
2013-10-31 22:58:14 +1300

    keep very small paragraphs together in shuffled text

--- shuffler.py
+++ shuffler.py
@@ -6,8 +6,21 @@ import re
 
 def shuffle(text):
     paragraphs = re.split("\n\s*\n\s*", text)
-    random.shuffle(paragraphs)
-    return '\n\n'.join(paragraphs)
+    snippets = []
+    p2 = None
+    for p in paragraphs:
+        if p2 is not None:
+            p2 += p
+            if len(p2) > 50:
+                snippets.append(p2)
+                p2 = None
+        elif len(p) > 50:
+            snippets.append(p)
+        else:
+            p2 = p
+
+    random.shuffle(snippets)
+    return '\n\n'.join(snippets)
 
 
 def main(infile, outfile):
