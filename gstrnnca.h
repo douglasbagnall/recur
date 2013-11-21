5e34bbeb9d080780550c8da6389a1269fcd55e57
Douglas Bagnall <douglas@halo.gen.nz>
2013-11-21 23:58:14 +1300

    rnnca: settings that are rather too colourful

--- gstrnnca.h
+++ gstrnnca.h
@@ -42,29 +42,21 @@ G_BEGIN_DECLS
 #define ARRAY_LEN(x) sizeof(x) / sizeof((x)[0])
 
 const int RNNCA_YUV_OFFSETS[] = {
-  -1, -1,   0, -1,   1, -1,
+            0, -1,
   -1,  0,            1,  0,
-  -1,  1,   0,  1,   1,  1
+            0,  1,
 };
 const int RNNCA_YUV_LEN = ARRAY_LEN(RNNCA_YUV_OFFSETS);
 
 #define USE_UV_ONLY_OFFSETS 1
 #define USE_Y_ONLY_OFFSETS 1
-#define USE_Y_MEAN_3_OFFSETS 1
+#define USE_Y_MEAN_3_OFFSETS 0
 
 #if USE_UV_ONLY_OFFSETS
 const int RNNCA_UV_ONLY_OFFSETS[] = {
+  -1, -1,          1, -1,
 
-  -2, -2,          2, -2,
-           0,  0,
-  -2,  2,          2,  2,
-  /*
-       -1, -2,  1, -2,
-  -2, -1,            2, -1,
-             0,0,
-  -2,  1,            2,  1,
-       -1,  2,  1,  2
-  */
+  -1,  1,          1,  1,
 };
 const int RNNCA_UV_ONLY_LEN = ARRAY_LEN(RNNCA_UV_ONLY_OFFSETS);
 #else
@@ -74,25 +66,12 @@ const int RNNCA_UV_ONLY_LEN = 0;
 
 #if USE_Y_ONLY_OFFSETS
 const int RNNCA_Y_ONLY_OFFSETS[] = {
-  0, 0
-  /*
        -1, -2,  1, -2,
   -2, -1,            2, -1,
-    //       0,0,
+             0,0,
   -2,  1,            2,  1,
        -1,  2,  1,  2
-  */
-  /*
-  -2, -2,          2, -2,
-           0,  0,
-  -2,  2,          2,  2,
-  */
 
-  /*
-  -2, -2,  0, -2,  2, -2,
-  -2,  0,          2,  0,
-  -2,  2,  0,  2,  2,  2,
-  */
 };
 const int RNNCA_Y_ONLY_LEN = ARRAY_LEN(RNNCA_Y_ONLY_OFFSETS);
 #else
