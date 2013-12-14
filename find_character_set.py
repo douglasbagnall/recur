e927f973370ddf1bab3a22b0f37e55a277a2f4aa
Douglas Bagnall <douglas@halo.gen.nz>
2013-12-14 22:46:58 +1300

    find_character_set.py prints short form with -q, and is unicode aware
    Unicode awareness via switching to python3.

--- find_character_set.py
+++ find_character_set.py
@@ -1,17 +1,29 @@
-#/usr/bin/python
+#!/usr/bin/python3
 from collections import Counter
 import sys
 
-def main(files):
+def main(files, short=False):
     chars = Counter()
     for fn in files:
         f = open(fn)
         s = ' '.join(f.read().split()).lower()
         f.close()
         chars.update(s)
-    i = 1
-    for c, n in chars.most_common():
-        print "%2d '%s' %20d" % (i, c, n)
-        i += 1
+    if short:
+        threshold = len(s) // 10000
+        print('%-30s: {%s}    {%s}' % (' '.join(files),
+                                       '' .join(c for c, n in chars.most_common()
+                                                if n >= threshold),
+                                       '' .join(c for c, n in chars.most_common()
+                                                if n < threshold)))
+    else:
+        i = 1
+        for c, n in chars.most_common():
+            print("%2d '%s' %10d %10.3g" % (i, c, n, float(n) / len(s)))
+            i += 1
 
-main(sys.argv[1:])
+if '-q' in sys.argv:
+    sys.argv.remove('-q')
+    main(sys.argv[1:], short=True)
+else:
+    main(sys.argv[1:])
