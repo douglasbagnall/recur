bd890686a1e60a4a264ae1059257f212bd20dced
Douglas Bagnall <douglas@halo.gen.nz>
2014-02-26 10:25:26 +1300

    kiwi-test: Ongoing gutting of kiwi-test/Classifier: not really working

--- classify.py
+++ classify.py
@@ -112,42 +112,38 @@ class BaseClassifier(object):
         self.mainloop.quit()
 
 
-    def get_results_counter(self):
+    def get_results_counter(self, members=2):
         groups = []
         for group in self.classes:
-            groups.append({x: [0,0] for x in group})
+            groups.append({x: [0] * members for x in group})
         return groups
 
 
 
 class Classifier(BaseClassifier):
-    pending_files = []
+    data = []
     quiet = False
 
-    def classify_files_with_timings(self, files, timings):
+    def classify(self, data):
         self.all_results = []
         self.class_results = self.get_results_counter()
-        self.pending_files = list(reversed(files))
-        self.pending_timings = list(reversed(timings))
+        self.data = list(reversed(data))
         self.classifier.set_property('training', False)
-        self.kiwi_probabilities = []
+        self.probabilities = self.get_results_counter(0)
         self.load_next_file()
         self.mainloop.run()
         return self.all_results
 
-
     def load_next_file(self):
         self.pipeline.set_state(Gst.State.READY)
-        fn = self.pending_files.pop()
-        timings = self.pending_timings.pop()
-
-        targets = ' '.join('c0t%.2f:%s' % (t, c) for c, t in timings)
-
+        fn, timings = self.data.pop()
+        targets = ' '.join(x[3] % 0 for x in timings)
+        #print fn, targets
         self.current_file = fn
         self.classifier.set_property("forget", 0)
         self.filesrcs[0].set_property('location', fn)
         self.classifier.set_property('target', targets)
-        self.file_results = []
+        self.file_results = [[] for x in self.classes]
         self.file_class_results = self.get_results_counter()
         self.pipeline.set_state(Gst.State.PLAYING)
 
@@ -156,79 +152,51 @@ class Classifier(BaseClassifier):
         s = msg.get_structure()
         if s.get_name() != "classify":
             return
-        print s.to_string()
+        #print s.to_string()
         v = s.get_value
-        results = []
         for i, group in enumerate(self.class_results):
             f_group = self.file_class_results[i]
             probs = self.probabilities[i]
-
-            correct = v('channel 0, group %d correct' % i)
-            winner = v('channel 0, group %d winner' % i)
-            target = v('channel 0, group %d target' % i)
+            key = 'channel 0, group %d ' % i
+            correct = v(key + 'correct')
+            winner = v(key + 'winner')
+            target = v(key + 'target')
             for k in group:
-                probs[k].append(v('channel 0, group %d %s' % (i, k)))
+                probs[k].append(v(key + k))
             group[target][correct] += 1
             f_group[target][correct] += 1
-            results.append(target, correct)
-
-        self.file_results.append(results)
+            self.file_results[i].append((target, correct))
 
 
     def report(self):
         self.pipeline.set_state(Gst.State.READY)
-        step = len(self.file_results) / 100.0
-        next_stop = 0
         out = []
-        i = 0
-        classes = self.classes
         colours = [COLOURS[x] for x in 'PPrrRRYYGgCC']
-        for target, correct in self.file_results:
-            if i >= next_stop:
-                if i:
-                    s = sum(current_targets)
-                    m = max(current_targets)
-                    if m > s * 0.9:
-                        c = classes[current_targets.index(m)]
-                    else:
-                        c = '~'
-                    colour = colours[int(current_correct * 10.01 / s)]
-                    #colour = COLOURS['PRrYgGC'[sum(current_correct > s * x for x in
-                    #                              (0.1, 0.25, 0.5, 0.66,
-                    #                               0.77, 0.88, 0.99)]]]
-                    out.append('%s%s' % (colour, c))
-                next_stop += step
-                current_correct = 0
-                current_targets = [0] * len(self.classes)
-            t, c = self.file_results[i]
-            current_correct += c
-            current_targets[t] += 1
-            i += 1
-        out.extend((COLOURS['Z'],))
 
-        i = 0
-        grand_total = 0
-        total_right = 0
-        for wrong, right in self.file_class_results:
-            total = wrong + right
-            if  total:
-                s = "%s: %d/%d (%.2g)" % (classes[i], right, total, float(right) / total)
-                n = int(right * 60.0 / total)
-                c = colours[right * 10 / total]
-                out.append("\n%-20s %s%s%s%s" %
-                           (s, c, '=' * n,
-                            COLOURS['Z'], '.' * (60 - n)))
+        for groupno, file_results in enumerate(self.file_results):
+            classes = self.classes[groupno]
+            step = len(file_results) / 100.0
+            next_stop = 0
+            for i, result in enumerate(file_results):
+                target, correct = result
+                t_index = classes.index(target)
+                if i >= next_stop:
+                    if i:
+                        s = sum(current_targets)
+                        m = max(current_targets)
+                        if m > s * 0.9:
+                            c = classes[current_targets.index(m)]
+                        else:
+                            c = '~'
+                        colour = colours[int(current_correct * 10.01 / s)]
+                        out.append('%s%s' % (colour, c))
+                    next_stop += step
+                    current_correct = 0
+                    current_targets = [0] * len(classes)
+                current_correct += correct
+                current_targets[t_index] += 1
 
-            else:
-                out.append("\n%s: %s / 0" % (classes[i], right))
-
-            i += 1
-            grand_total += total
-            total_right += right
-
-
-        print '\nfile://%s%s%s' % (colours[total_right * 10 / grand_total],
-                                 self.current_file, COLOURS['Z'])
+            out.extend((COLOURS['Z'],'\n'))
 
         print ''.join(out)
         #print self.file_class_results
@@ -237,97 +205,82 @@ class Classifier(BaseClassifier):
 
 
     def show_roc_curve(self):
-        if NO_ROC:
-            return
         import matplotlib.pyplot as plt
-        kp = sorted(self.kiwi_probabilities)
-        self.kiwi_probabilities = []
-        sum_true = sum(1 for x in kp if x[1])
-        sum_false = len(kp) - sum_true
+        for probabilities in self.probabilities:
+            kp = sorted(probabilities).values()
+            del probabilities[:]
+            sum_true = sum(1 for x in kp if x[1])
+            sum_false = len(kp) - sum_true
 
-        tp_scale = 1.0 / (sum_true or 1)
-        fp_scale = 1.0 / (sum_false or 1)
-        tp = []
-        fp = []
-        false_positives = sum_false
-        true_positives = sum_true
-        half = 0
-        ax, ay, ad, ap = 0, 0, 0, 0
-        bx, by, bd, bp = 0, 0, 99, 0
-        cx, cy, cd, cp = 0, 0, 0, 0
-        for prob, truth in kp:
-            false_positives -= not truth
-            true_positives -= truth
-            x = false_positives * fp_scale
-            y = true_positives * tp_scale
-            half += prob < 0.5
-            d = (1 - x) * (1 - x) + y * y
-            if d > ad:
-                ad = d
-                ax = x
-                ay = y
-                ap = prob
-            d = x * x + (1 - y) * (1 - y)
-            if d < bd:
-                bd = d
-                bx = x
-                by = y
-                bp = prob
-            d = y - x
-            if d > cd:
-                cd = d
-                cx = x
-                cy = y
-                cp = prob
-            fp.append(x)
-            tp.append(y)
+            tp_scale = 1.0 / (sum_true or 1)
+            fp_scale = 1.0 / (sum_false or 1)
+            tp = []
+            fp = []
+            false_positives = sum_false
+            true_positives = sum_true
+            half = 0
+            ax, ay, ad, ap = 0, 0, 0, 0
+            bx, by, bd, bp = 0, 0, 99, 0
+            cx, cy, cd, cp = 0, 0, 0, 0
+            for prob, truth in kp:
+                false_positives -= not truth
+                true_positives -= truth
+                x = false_positives * fp_scale
+                y = true_positives * tp_scale
+                half += prob < 0.5
+                d = (1 - x) * (1 - x) + y * y
+                if d > ad:
+                    ad = d
+                    ax = x
+                    ay = y
+                    ap = prob
+                d = x * x + (1 - y) * (1 - y)
+                if d < bd:
+                    bd = d
+                    bx = x
+                    by = y
+                    bp = prob
+                d = y - x
+                if d > cd:
+                    cd = d
+                    cx = x
+                    cy = y
+                    cp = prob
+                fp.append(x)
+                tp.append(y)
 
-        if half < len(fp):
-            hx = (fp[half - 1] + fp[half]) * 0.5
-            hy = (tp[half - 1] + tp[half]) * 0.5
-        else:
-            hx = fp[half - 1]
-            hy = tp[half - 1]
+            if half < len(fp):
+                hx = (fp[half - 1] + fp[half]) * 0.5
+                hy = (tp[half - 1] + tp[half]) * 0.5
+            else:
+                hx = fp[half - 1]
+                hy = tp[half - 1]
 
 
-        fp.reverse()
-        tp.reverse()
-        print "best", bx, by
-        print "half", hx, hy
-        plt.plot(fp, tp)
-        plt.annotate("0.5", (hx, hy), (0.4, 0.4),
-                     arrowprops={'width':1, 'color': '#00cc00'})
-        plt.annotate("furthest from all bad %.2g" % ap, (ax, ay), (0.3, 0.3),
-                     arrowprops={'width':1, 'color': '#00cccc'},
-                     )
-        plt.annotate("closest to all good %.2g" % bp, (bx, by), (0.6, 0.6),
-                     arrowprops={'width':1, 'color': '#cc0000'},
-                     )
-        plt.annotate("furthest from diagonal %.2g" % cp, (cx, cy), (0.5, 0.5),
-                     arrowprops={'width':1, 'color': '#aa6600'},
-                     )
+            fp.reverse()
+            tp.reverse()
+            print "best", bx, by
+            print "half", hx, hy
+            plt.plot(fp, tp)
+            plt.annotate("0.5", (hx, hy), (0.4, 0.4),
+                         arrowprops={'width':1, 'color': '#00cc00'})
+            plt.annotate("furthest from all bad %.2g" % ap, (ax, ay), (0.3, 0.3),
+                         arrowprops={'width':1, 'color': '#00cccc'},
+                         )
+            plt.annotate("closest to all good %.2g" % bp, (bx, by), (0.6, 0.6),
+                         arrowprops={'width':1, 'color': '#cc0000'},
+                         )
+            plt.annotate("furthest from diagonal %.2g" % cp, (cx, cy), (0.5, 0.5),
+                         arrowprops={'width':1, 'color': '#aa6600'},
+                         )
 
-        plt.axes().set_aspect('equal')
+            plt.axes().set_aspect('equal')
         plt.show()
 
     def on_eos(self, bus, msg):
-        self.report()
-        if not self.pending_files:
-            i = 0
-            gt = 0
-            for wrong, right in self.class_results:
-                t = wrong + right
-                gt += t
-                if t:
-                    print "%s: %s/%s  %.2f" % (self.classes[i], right, t,
-                                               float(right) / t)
-                else:
-                    print "%s: %s/%s  ---" % (self.classes[i], right, t)
-                i += 1
-            print "%s windows == %.2f minutes" % (gt,
-                                                  gt * WINDOW_SIZE / (2.0 * 60 * 8000))
+        #self.report()
+        if not self.data:
             self.show_roc_curve()
-
             self.stop()
         else:
             self.load_next_file()
