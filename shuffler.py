4f620ac981642360738d1c5d8b628cabc4801632
Douglas Bagnall <douglas@halo.gen.nz>
2014-05-13 22:23:50 +1200

    shuffler: shuffle text in larger minimum chunks (300 vs 80)
    There are longer range relationships that *might* be capturable.  The
    reason for the initial short span is to make the validation set unbiased
    when small. The validation set used to be very small because validation
    happened every 1024 characters. Now it tends to be used less often using
    larger sets.

--- shuffler.py
+++ shuffler.py
@@ -4,19 +4,19 @@ import random
 import os, sys
 import re
 
-MIN_PARA_SIZE = 80
+MIN_PARA_SIZE = 300
 
-def shuffle(text):
+def shuffle(text, min_para_size):
     paragraphs = re.split("\r?\n\s*\n\s*", text)
     snippets = []
     p2 = None
     for p in paragraphs:
         if p2 is not None:
             p2 += "\n" + p
-            if len(p2) > MIN_PARA_SIZE:
+            if len(p2) > min_para_size:
                 snippets.append(p2)
                 p2 = None
-        elif len(p) > MIN_PARA_SIZE:
+        elif len(p) > min_para_size:
             snippets.append(p)
         else:
             p2 = p
@@ -25,9 +25,9 @@ def shuffle(text):
     return '\n\n'.join(snippets)
 
 
-def main(infile, outfile):
+def main(infile, outfile, min_para_size=MIN_PARA_SIZE):
     f = open(infile)
-    s = shuffle(f.read())
+    s = shuffle(f.read(), int(min_para_size))
     f.close()
     f = open(outfile, 'w')
     f.write(s)
