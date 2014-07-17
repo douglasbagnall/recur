6ac3049be190839315dbad5c3037bbbb7b788c63
Douglas Bagnall <douglas@halo.gen.nz>
2014-07-17 13:50:15 +1200

    charmodel: shift common inline functions to charmodel-helpers.h

---
+++ charmodel-helpers.h
@@ -0,0 +1,31 @@
+#include "recur-nn.h"
+#include "recur-nn-helpers.h"
+#include <math.h>
+#include "path.h"
+#include "badmaths.h"
+#include "charmodel.h"
+
+static inline float
+capped_log2f(float x){
+  return (x < 1e-30f) ? -100.0f : log2f(x);
+}
+
+static inline float*
+one_hot_opinion(RecurNN *net, int hot){
+  float *inputs;
+  int len;
+  if (net->bottom_layer){
+    inputs = net->bottom_layer->inputs;
+    len = net->bottom_layer->input_size;
+  }
+  else{
+    inputs = net->real_inputs;
+    len = net->input_size;
+  }
+
+  //XXX could just set the previous one to zero (i.e. remember it)
+  memset(inputs, 0, len * sizeof(float));
+  inputs[hot] = 1.0f;
+  return rnn_opinion(net, NULL);
+}
+
--- charmodel-predict.c
+++ charmodel-predict.c
@@ -11,33 +11,10 @@ This uses the RNN to predict the next character in a text sequence.
 #include <stdio.h>
 
 #include "charmodel.h"
+#include "charmodel-helpers.h"
 #include "utf8.h"
 
 static inline float
-capped_log2f(float x){
-  return (x < 1e-30f) ? -100.0f : log2f(x);
-}
-
-static inline float*
-one_hot_opinion(RecurNN *net, int hot){
-  float *inputs;
-  int len;
-  if (net->bottom_layer){
-    inputs = net->bottom_layer->inputs;
-    len = net->bottom_layer->input_size;
-  }
-  else{
-    inputs = net->real_inputs;
-    len = net->input_size;
-  }
-
-  //XXX could just set the previous one to zero (i.e. remember it)
-  memset(inputs, 0, len * sizeof(float));
-  inputs[hot] = 1.0f;
-  return rnn_opinion(net, NULL);
-}
-
-static float
 net_error_bptt(RecurNN *net, float *restrict error, int c, int next, int *correct){
   ASSUME_ALIGNED(error);
   float *answer = one_hot_opinion(net, c);
