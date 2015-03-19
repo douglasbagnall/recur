66f6c9bd3aef434ccdec1fb21b39a2240365aa5d
Douglas Bagnall <douglas@halo.gen.nz>
2015-03-19 22:24:00 +1300

    experimentally add a heap of function attributes to malloc_or_die

--- recur-common.h
+++ recur-common.h
@@ -90,7 +90,8 @@ typedef unsigned int uint;
 
 /*memory allocation. It needs to be SSE aligned */
 #define ALIGNMENT 16
-static inline __attribute__((malloc)) void *
+static inline __attribute__((__malloc__, __assume_aligned__(16),
+        __alloc_size__(1), __nonnull__)) void *
 malloc_aligned_or_die(size_t size){
   void *mem;
   if (sizeof(void*) * 2 >= ALIGNMENT){
