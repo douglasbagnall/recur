5bfde963dc896d12f2819029c4b75d0b4532dbb1
Douglas Bagnall <douglas@halo.gen.nz>
2015-04-07 17:54:35 +1200

    py-charmodel setup.py: better optimisation options
    -DVECTOR and -O3/-Ofast are the main ones.

--- setup.py
+++ setup.py
@@ -15,8 +15,10 @@ setup(
                      "charmodel-helpers.h", "path.h", "py-recur-text.h",
                      "recur-common.h", "recur-nn.h"],  # ..., etc.
             include_dirs = ["."],
+            define_macros = [('_GNU_SOURCE', None),
+                             ('VECTOR', None)],
             extra_compile_args=['-ggdb', '-std=gnu11', '-I.', '-Wall',
-                                '-D_GNU_SOURCE'],
+                                '-Ofast', '-ffast-math', '-fno-inline'],
             language="c"
         )
     ]
