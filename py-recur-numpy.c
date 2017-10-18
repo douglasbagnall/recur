9aa390658aed0eb78f05c513fa2708728dcac497
Douglas Bagnall <douglas@halo.gen.nz>
2017-10-19 12:08:22 +1300

    rnnumpy: log a thing called rolling_accuracy

--- py-recur-numpy.c
+++ py-recur-numpy.c
@@ -387,6 +387,7 @@ Net_train(Net *self, PyObject *args, PyObject *kwds)
         bptt->learn_rate = learn_rate;
     }
 
+    float rolling_accuracy = 0.5;
     for (epoch = 1; epoch <= n_epochs; epoch++) {
         uint countdown = self->batch_size;
 	for (i = 0; i < shape[0]; i++) {
@@ -418,10 +419,14 @@ Net_train(Net *self, PyObject *args, PyObject *kwds)
             }
 
 	    softmax_best_guess(error, answer, net->output_size);
+            rolling_accuracy *= 255.0;
 	    for (j = 0; j < net->output_size; j++) {
 		error[j] += trow[j];
+                    rolling_accuracy += fabsf(error[j]) < 0.5;
 	    }
+            rolling_accuracy /= 256.0;
             //rnn_bptt_calculate(net, 100);
+            rnn_log_float(net, "rolling_accuracy", rolling_accuracy);
             countdown--;
 	    if (countdown == 0) {
 		rnn_apply_learning(net, self->learning_method, self->momentum);
