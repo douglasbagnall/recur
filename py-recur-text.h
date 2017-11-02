9f1fafc1f0c807a6b991882b3e4da28bd3131a9c
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2017-11-02 15:19:35 +1300

    py-recur-text.h: add header guard ifdefs

--- py-recur-text.h
+++ py-recur-text.h
@@ -1,7 +1,10 @@
 /* Copyright 2015 Douglas Bagnall <douglas@halo.gen.nz> LGPL
 
    Python bindings for text RNNs. */
+#ifndef __PY_RECUR_TEXT_H__
+#define __PY_RECUR_TEXT_H__
 
 #include <Python.h>
 #include "structmember.h"
 #include "structseq.h"
+#endif
